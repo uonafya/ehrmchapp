53be97b0d073f8869a3fb16555bb252acc92df5e
ningosi <nicotwendelee@gmail.com>
2020-09-20 01:52:42 +0300

    Package optimization in the activator class

--- api/src/main/java/org/openmrs/module/mchapp/MCHAppActivator.java
+++ api/src/main/java/org/openmrs/module/mchapp/MCHAppActivator.java
@@ -15,7 +15,6 @@ package org.openmrs.module.mchapp;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.ModuleActivator;
 
 /**
