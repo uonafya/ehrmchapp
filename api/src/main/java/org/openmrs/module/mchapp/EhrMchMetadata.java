044aa33311eb84f4bd85457b00fc0c75d9fdd7b7
bushisky <georgeodhiambo347@gmail.com>
2020-09-21 12:22:01 +0300

    Code clean-up

--- api/src/main/java/org/openmrs/module/mchapp/EhrMchMetadata.java
+++ api/src/main/java/org/openmrs/module/mchapp/EhrMchMetadata.java
@@ -20,6 +20,7 @@ public class EhrMchMetadata extends AbstractMetadataBundle {
 		
 		public static final String LAST_MENSTRUAL_PERIOD = "1427AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
 	}
+	
 	//reached here for concept uuid confirmation
 	public static final class _VistTypes {
 		
