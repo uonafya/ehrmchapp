4b6072c8d04f0dd29b20ca84e075ed253cb67228
ningosi <nicotwendelee@gmail.com>
2022-03-25 12:17:17 +0300

    Provide correct concepts for mch clinic

--- api/src/main/java/org/openmrs/module/mchapp/MchProfileConcepts.java
+++ api/src/main/java/org/openmrs/module/mchapp/MchProfileConcepts.java
@@ -13,13 +13,13 @@ public class MchProfileConcepts {
 	    , "299AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" //vdrl
 	    , "21AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" //hb
 	    , "1356AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" //HIV Test
-	    , "395a5756-81dc-4964-99a6-060d1620d101" //HIV Re-test
+	    , "1356AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" //HIV Re-test
 	);
 	
 	public static final List<String> PNC_PROFILE_CONCEPTS = Arrays.asList("5599AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", //date of delivery
-	    "12bce98b-f1da-45a0-85b8-82da350bd04d", // place of delivery
-	    "6ac1fd9c-e696-4f46-9ec3-5be0b06e07dd", //mode of delivery
-	    "5ddb1a3e-0e88-426c-939a-abf4776b024a" //state of baby
+	    "1572AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", // place of delivery
+	    "5630AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA", //mode of delivery
+	    "8f3ed313-e2ea-48df-b211-69bb3f232b20" //state of baby
 	);
 	
 }
